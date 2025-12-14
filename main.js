0fff96440a9ff58dd3ad76c1285a413b51602baf
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2025-12-14 20:49:38 +0100

    add: funcionalidad de musica y sonido configurable - gitignore assets

--- main.js
+++ main.js
@@ -1,1 +1,130 @@
 /* JS para la pagina de bienvenida */
+
+/* --- MANEJO SOUNDTRACK ---*/
+const playlist = [
+    "assets/music/01 - Opening.ogg",
+    "assets/music/03 - HWV 56 - Why do the nations so furiously rage together.ogg",
+    "assets/music/04 - Sanctuary.ogg"
+];
+
+// variables musica
+let currentTrack = 0;
+const music = new Audio();
+music.volume = 0.2;
+music.muted = true;
+
+function playTrack(index) {
+    music.src = playlist[index];
+    music.play();
+}
+
+function pauseTrack(index) {
+    music.src = playlist[index];
+    music.pause();
+}
+
+/* MANEJO SONIDO */
+let soundsEnabled = false;
+
+const soundPool = {
+    hover: new Audio("assets/sounds/Mouth_Special_00.mp3"),
+    click: new Audio("assets/sounds/Click_Electronic_04.mp3"),
+    back: new Audio("assets/sounds/Mouth_Special_00.mp3"),
+    start: new Audio("assets/sounds/Mouth_Special_00.mp3"),
+};
+
+function playSound(name) {
+    if (!soundsEnabled) return;
+    const sound = soundPool[name].cloneNode(); // clona para poder reproducir múltiples a la vez
+    sound.volume = 0.1;
+    sound.play();
+}
+
+
+// Cuando termina una canción pasa a la siguiente
+music.addEventListener("ended", () => {
+    currentTrack++;
+    if (currentTrack >= playlist.length) {
+        currentTrack = 0; // vuelve al inicio
+    }
+    playTrack(currentTrack);
+});
+
+
+/* --- CONFIGURACIÓN DEL JUEGO --- */
+const gameConfig = {
+    players: 2,
+    difficulty: "NORMAL",
+    player1: {
+        name: "PLAYER1",
+        controls: {
+            up: "W",
+            down: "S",
+            action: "Space"
+        }
+    },
+    player2: {
+        name: "PLAYER2",
+        controls: {
+            up: "ArrowUp",
+            down: "ArrowDown",
+            action: "Enter"
+        }
+    }
+}
+
+/* --- CARGA DINÁMICA DE MENÚS --- */
+const menuPanel = document.getElementById("menu-panel");
+
+function setupMenuEvents() {
+    menuPanel.addEventListener("click", (e) => {
+        playSound("click");
+        if (e.target.closest("#music-switch")) {
+            music.muted = !music.muted;
+            playTrack(currentTrack);
+            document.getElementById("music-switch-value").textContent = music.muted ? "OFF" : "ON";
+        }
+        if (e.target.closest("#sounds-switch")) {
+            soundsEnabled = !soundsEnabled;
+            document.getElementById("sounds-switch-value").textContent = soundsEnabled ? "ON" : "OFF";
+        }
+    });
+
+    menuPanel.addEventListener("mouseover", (e) => {
+        const li = e.target.closest(".menu-item");
+        if (!li || !menuPanel.contains(li)) return;
+
+        /* e.relatedTarget es el elemento desde el que entró,
+        si el li lo contiene(es un hijo de li) return */
+        if (li.contains(e.relatedTarget)) return;
+
+        playSound("hover");
+    });
+}
+
+
+async function loadMenu(menuFile) {
+    const res = await fetch(`menus/${menuFile}`);
+    menuPanel.innerHTML = await res.text();
+}
+
+/**
+ * TODO
+ * @param {*} config 
+ */
+async function loadGame(config) {
+    const res = await fetch("pong.html");
+    menuPanel.innerHTML = await res.text();
+    // TODO
+    //setupGameEvents();
+}
+
+// Menú inicial
+loadMenu("main-menu.html");
+
+// setup de eventos de menu
+setupMenuEvents();
+
+// Exponer funciones globales para los botones
+window.showMainMenu = () => loadMenu("main-menu.html");
+window.showOptionsMenu = () => loadMenu("options-menu.html");
